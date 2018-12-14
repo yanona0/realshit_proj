c1efc6f32e006cd992c67c87eb4d71117c52c4ce
HomoSocialisticus <44208358+HomoSocialisticus@users.noreply.github.com>
2018-12-14 21:13:24 +0300

    build3(added modified platforms)
    Теперь на некоторые платформы можно запрыгивать вертикально снизу

---
+++ build3/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController2D contoller;
+    public float runSpeed = 30f;
+    bool jump = false;
+    float horizontalMove = 0f;
+	
+	void Update () {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        contoller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
